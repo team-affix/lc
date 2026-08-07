ee5f28fdcdc7cf8a77e59bd2db35858f792cac1a
teamaffixmb-jake <teamaffixmb.jake@gmail.com>
2026-08-07 01:15:14 -0700

    Updated formatting rules

--- core/hpp/infrastructure/evaluator.hpp
+++ core/hpp/infrastructure/evaluator.hpp
@@ -1,20 +1,22 @@
 #ifndef EVALUATOR_HPP
 #define EVALUATOR_HPP
 
-#include <cstdint>
-#include <variant>
+#include "debug_assert.hpp"
 #include "value_objects/env.hpp"
 #include "value_objects/expr.hpp"
 #include "value_objects/val.hpp"
-#include "debug_assert.hpp"
+#include <cstdint>
+#include <variant>
 
-template<typename IMakeClo, typename IMakeNapp, typename IMakeDelayed>
+template <typename IMakeClo, typename IMakeNapp, typename IMakeDelayed>
 struct evaluator {
-    evaluator(IMakeClo& make_clo, IMakeNapp& make_napp, IMakeDelayed& make_delayed);
+    evaluator(IMakeClo& make_clo, IMakeNapp& make_napp,
+              IMakeDelayed& make_delayed);
     const val* eval(const expr* term, const env* e);
 
-private:
-    const val* eval_app(const val* fun_val, const expr* arg, const env* arg_env);
+  private:
+    const val* eval_app(const val* fun_val, const expr* arg,
+                        const env* arg_env);
     const val* lookup(uint32_t index, const env* e);
 
     IMakeClo& make_clo_;
@@ -22,29 +24,30 @@ private:
     IMakeDelayed& make_delayed_;
 };
 
-template<typename IMakeClo, typename IMakeNapp, typename IMakeDelayed>
+template <typename IMakeClo, typename IMakeNapp, typename IMakeDelayed>
 evaluator<IMakeClo, IMakeNapp, IMakeDelayed>::evaluator(
     IMakeClo& make_clo, IMakeNapp& make_napp, IMakeDelayed& make_delayed)
-    : make_clo_(make_clo), make_napp_(make_napp), make_delayed_(make_delayed) {}
+    : make_clo_(make_clo), make_napp_(make_napp), make_delayed_(make_delayed) {
+}
 
-template<typename IMakeClo, typename IMakeNapp, typename IMakeDelayed>
+template <typename IMakeClo, typename IMakeNapp, typename IMakeDelayed>
 const val* evaluator<IMakeClo, IMakeNapp, IMakeDelayed>::eval(const expr* term,
-                                                             const env* e) {
-    if (const expr::var* variable = std::get_if<expr::var>(&term->content)) {
+                                                              const env* e) {
+    if(const expr::var* variable = std::get_if<expr::var>(&term->content)) {
         DEBUG_ASSERT(e != nullptr);
         return lookup(variable->index, e);
     }
-    if (const expr::abs* abstraction = std::get_if<expr::abs>(&term->content))
+    if(const expr::abs* abstraction = std::get_if<expr::abs>(&term->content))
         return make_clo_.make_clo(abstraction->body, e);
     const expr::app& application = std::get<expr::app>(term->content);
     const val* fun_val = eval(application.fun, e);
     return eval_app(fun_val, application.arg, e);
 }
 
-template<typename IMakeClo, typename IMakeNapp, typename IMakeDelayed>
+template <typename IMakeClo, typename IMakeNapp, typename IMakeDelayed>
 const val* evaluator<IMakeClo, IMakeNapp, IMakeDelayed>::eval_app(
     const val* fun_val, const expr* arg, const env* arg_env) {
-    if (const val::clo* closure = std::get_if<val::clo>(&fun_val->content)) {
+    if(const val::clo* closure = std::get_if<val::clo>(&fun_val->content)) {
         const env* extended =
             make_delayed_.make_delayed(arg, arg_env, closure->captured);
         return eval(closure->body, extended);
@@ -52,12 +55,12 @@ const val* evaluator<IMakeClo, IMakeNapp, IMakeDelayed>::eval_app(
     return make_napp_.make_napp(fun_val, arg, arg_env);
 }
 
-template<typename IMakeClo, typename IMakeNapp, typename IMakeDelayed>
+template <typename IMakeClo, typename IMakeNapp, typename IMakeDelayed>
 const val* evaluator<IMakeClo, IMakeNapp, IMakeDelayed>::lookup(uint32_t index,
-                                                               const env* e) {
+                                                                const env* e) {
     DEBUG_ASSERT(e != nullptr);
-    if (index == 0) {
-        if (const env::ready* bound = std::get_if<env::ready>(&e->binder))
+    if(index == 0) {
+        if(const env::ready* bound = std::get_if<env::ready>(&e->binder))
             return bound->value;
         const env::delayed& thunk = std::get<env::delayed>(e->binder);
         const val* forced = eval(thunk.arg, thunk.arg_env);
--- core/hpp/infrastructure/normalizer.hpp
+++ core/hpp/infrastructure/normalizer.hpp
@@ -1,25 +1,24 @@
 #ifndef NORMALIZER_HPP
 #define NORMALIZER_HPP
 
-#include "value_objects/env.hpp"
 #include "value_objects/expr.hpp"
 #include "value_objects/val.hpp"
 
-template<typename IEval, typename IReify>
-struct normalizer {
+template <typename IEval, typename IReify> struct normalizer {
     normalizer(IEval& eval, IReify& reify);
     const expr* normalize(const expr* term);
 
-private:
+  private:
     IEval& eval_;
     IReify& reify_;
 };
 
-template<typename IEval, typename IReify>
+template <typename IEval, typename IReify>
 normalizer<IEval, IReify>::normalizer(IEval& eval, IReify& reify)
-    : eval_(eval), reify_(reify) {}
+    : eval_(eval), reify_(reify) {
+}
 
-template<typename IEval, typename IReify>
+template <typename IEval, typename IReify>
 const expr* normalizer<IEval, IReify>::normalize(const expr* term) {
     const val* whnf = eval_.eval(term, nullptr);
     return reify_.reify(whnf, 0);
